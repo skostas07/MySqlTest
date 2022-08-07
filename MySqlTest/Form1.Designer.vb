cc2cc658325a2ed7306682bd0e13f7482396ce26
skostas <skostas@DELL10PRO>
2022-08-07 13:54:37 +0300

    Proje dosyası ekle.

---
+++ MySqlTest/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.Location = New System.Drawing.Point(139, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(241, 63)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connect To Mysql"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(523, 165)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Connect To MySQL"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ MySqlTest/Form1.vb
@@ -0,0 +1,27 @@
+﻿
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conn As New MySql.Data.MySqlClient.MySqlConnection
+        Dim myConnectionString As String
+
+        myConnectionString = "server=127.0.0.1;" _
+                    & "uid=root;" _
+                    & "pwd=;" _
+                    & "database=mysqltest"
+
+        Try
+            conn.ConnectionString = myConnectionString
+            conn.Open()
+            MessageBox.Show("Connected Database: " & conn.Database.ToString, "Connected!")
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            MessageBox.Show(ex.Message)
+        End Try
+
+        If conn.State = ConnectionState.Open Then
+            conn.Close()
+            MessageBox.Show("Connection Closed !", "Connecttion Lost")
+        End If
+    End Sub
+End Class
